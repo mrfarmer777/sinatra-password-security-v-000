2969e5f858f4a63794b9905a6a846d8a65a19275
Matt Farmer <mrfarmer777@gmail.com>
2018-06-29 19:04:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,8 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		#your code here!
-		user = User.find)by(:username=>params[:username])
+		user = User.find_by(:username=>params[:username])
+		
 		
 	end
 
